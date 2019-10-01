23b58d7e7e898ff4898a98cb3258167a47abe455
Nicola <nicolacastellani92@gmail.com>
2019-10-01 14:21:11 +0200

    Ts config

--- src/gameScripts/entities/enemies.prototype.ts
+++ src/gameScripts/entities/enemies.prototype.ts
@@ -12,11 +12,11 @@ Enemies.prototype = {
   enemiesWrapper: null,
   TARGET_ENEMIES_POSITION: {
     left: 0,
-    top: 0
+    top: 0,
   },
   ENEMIES_DIRECTION: 1,
   ENEMIES_SPEED: 2000,
-  checkCollision: function (element) {
+  checkCollision: function(element) {
     const { top, left } = element.getBoundingClientRect();
     let collided = document.elementsFromPoint(left, top);
     collided = collided.filter(c => c.className.indexOf('Player') > -1);
@@ -30,7 +30,7 @@ Enemies.prototype = {
     }
     return (collided || []).length > 0;
   },
-  move: function () {
+  move: function() {
     let { ENEMIES_DIRECTION, enemiesWrapper } = this;
     /// we need to move the plaeyr into a specific direction
     const targetMove = 25 * ENEMIES_DIRECTION;
@@ -40,8 +40,14 @@ Enemies.prototype = {
     let { offsetLeft } = enemiesWrapper;
     /// change it according to current direction
     offsetLeft += targetMove;
-    this.TARGET_ENEMIES_POSITION.left = Math.min(MAX, Math.max(MIN, offsetLeft));
-    if (this.TARGET_ENEMIES_POSITION.left >= MAX - 25 || this.TARGET_ENEMIES_POSITION.left <= MIN + 25) {
+    this.TARGET_ENEMIES_POSITION.left = Math.min(
+      MAX,
+      Math.max(MIN, offsetLeft),
+    );
+    if (
+      this.TARGET_ENEMIES_POSITION.left >= MAX - 25 ||
+      this.TARGET_ENEMIES_POSITION.left <= MIN + 25
+    ) {
       this.ENEMIES_DIRECTION *= -1;
       setTimeout(() => {
         this.TARGET_ENEMIES_POSITION.top += 16 * 3;
@@ -53,13 +59,21 @@ Enemies.prototype = {
       this.move();
     }, this.ENEMIES_SPEED);
   },
-  render: function () {
+  render: function() {
     const { enemiesWrapper, TARGET_ENEMIES_POSITION } = this;
     /// the enemies must be rendered in a simple way
     /// moving them from left to right and viceversa
-    /// let's start with the simplest case - move to left each {x} seconds 
-    let targetPosLeft = lerp(enemiesWrapper.offsetLeft, TARGET_ENEMIES_POSITION.left, .25);
-    let targetPosTop = lerp(enemiesWrapper.offsetTop, TARGET_ENEMIES_POSITION.top, .25);
+    /// let's start with the simplest case - move to left each {x} seconds
+    let targetPosLeft = lerp(
+      enemiesWrapper.offsetLeft,
+      TARGET_ENEMIES_POSITION.left,
+      0.25,
+    );
+    let targetPosTop = lerp(
+      enemiesWrapper.offsetTop,
+      TARGET_ENEMIES_POSITION.top,
+      0.25,
+    );
     enemiesWrapper.style.left = `${targetPosLeft}px`;
     enemiesWrapper.style.top = `${targetPosTop}px`;
   },
@@ -67,7 +81,8 @@ Enemies.prototype = {
     const { enemiesWrapper } = this;
     const bullet = document.createElement('div');
     const enemies = enemiesWrapper.querySelectorAll('.Enemy');
-    const randomEnemy = enemies[Math.floor(Math.random() * enemies.length - 1) + 1];
+    const randomEnemy =
+      enemies[Math.floor(Math.random() * enemies.length - 1) + 1];
     const { offsetLeft, offsetTop } = randomEnemy;
     bullet.classList.add('EnemyBullet');
     bullet.style.left = `${offsetLeft + enemiesWrapper.offsetLeft + 21.5}px`;
@@ -89,25 +104,27 @@ Enemies.prototype = {
       }
     }, 10);
   },
-  spawnRandom: function () {
+  spawnRandom: function() {
     let result = '';
     for (let i = 0; i < Math.floor(Math.random() * 6 + 3); i++) {
       for (let j = 0; j < 18; j++) {
-        result += `<div class="Enemy">${Math.floor(Math.random() * 3) > 1 ? '👽' : '👾'}</div>`
+        result += `<div class="Enemy">${
+          Math.floor(Math.random() * 3) > 1 ? '👽' : '👾'
+        }</div>`;
       }
     }
     this.enemiesWrapper.innerHTML = result;
   },
-  init: function () {
+  init: function() {
     this.enemiesWrapper = document.querySelector('.Enemies');
-    TARGET_ENEMIES_POSITION = {
+    this.TARGET_ENEMIES_POSITION = {
       left: this.enemiesWrapper.offsetLeft,
-      top: this.enemiesWrapper.offsetTop
+      top: this.enemiesWrapper.offsetTop,
     };
     this.spawnRandom();
     this.move();
     setInterval(() => {
       this.shoot();
     }, 1000);
-  }
-}
+  },
+};
